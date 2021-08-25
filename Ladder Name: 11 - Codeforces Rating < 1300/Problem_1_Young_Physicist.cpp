4efc5dee142c1ffd9c84acc6b157c708cdf17239
SachinSarin <60125906+SachinSarin@users.noreply.github.com>
2021-08-25 14:30:50 +0530

    Update Problem_1_Young_Physicist.cpp

--- Ladder Name: 11 - Codeforces Rating < 1300/Problem_1_Young_Physicist.cpp
+++ Ladder Name: 11 - Codeforces Rating < 1300/Problem_1_Young_Physicist.cpp
@@ -24,41 +24,32 @@ outputCopy
 YES
 
 */
+
 #include <iostream>
-#include <algorithm>
-#include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-	int n;
-	cin>>n;
-	int arr[n][n];
-	for(int i = 0; i < n; i++)
-	{
-	    for(int j = 0; j < n; j++)
-	    {
-	        cin>>arr[i][j];
-	    }
-	}
-	
-	int sum = 0;
-	for(int i = 0; i < n; i++)
-	{
-	    for(int j = 0; j < n; j++)
-	    {
-	        sum += arr[i][j];
-	    }
-	}
-	if(sum==0)
-	{
-	    cout<<"YES"<<endl;
-	    
-	}
-	else
-	{
-	    cout<<"NO"<<endl;
-	}
-	
-	
-	return 0;
+int main()
+{
+    int n, x, y, z, xsum(0), ysum(0), zsum(0);
+    cin >> n;
+
+    while (n--)
+    {
+        cin >> x >> y >> z;
+        xsum += x;
+        ysum += y;
+        zsum += z;
+    }
+
+    if (xsum == 0 && ysum == 0 && zsum == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
 }
